e3106f2175c5e71e2a1db5a571f95350392ee98e
JonasGrunau <grunaujonas@gmail.com>
2018-11-14 02:13:28 +0100

    commit

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {createStackNavigator} from "react-navigation";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import UserListScreen from "./screens/UserListScreen/UserListScreen";
+import UserProfileScreen from "./screens/UserProfileScreen/UserProfileScreen";
 
 const StackNavigation = createStackNavigator({
     Home: HomeScreen,
